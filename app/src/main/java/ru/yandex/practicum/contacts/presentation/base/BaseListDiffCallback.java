63740207e05ee2595759f14896aac2cf34ea29fb
user <vladislavkhamin90@gmail.com>
2024-11-20 17:59:21 +0300

    Проектная работа №2

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -18,7 +18,19 @@ public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUt
     @Override
     @Nullable
     public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
-        return oldItem.equals(newItem);
+        return newItem;
     }
-
 }
+/*
+*Немного не понял требование в последнем ревью касательно этого пункта:
+*1. Выполнение требований задания:
+   ✅❌ Создан дженерик-класс BaseListDiffCallback с реализацией
+ *
+* Класс BaseListDiffCallback создан и реализован в классах
+* FilterContactTypeAdapter
+* ContactAdapter
+* SortTypeAdapter
+* как в задании. После всех исправлений приложение компилируется, контакты сортируются и фильтруются.
+* Прошу указать гду допустил ошибку. Заранее спасибо.
+*
+* */
